fde858d573ce4a89d9a26a028443c3ca992caa8b
Shafin580 <shafinarits580@gmail.com>
2024-10-05 18:37:08 +0600

    - Defined User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,23 @@ class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
+    // Granting Access to Columns to be filled when creating a new entry or updating an existing entry
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'activation_token', 'is_active',
     ];
 
+    // When Fetching Data, These Columns will not show data
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // Data Casting when fetching data
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'is_active' => 'boolean',
+    ];
+
+    // Function To Return the unique identifier of this user 
     public function getJWTIdentifier()
     {
         return $this->getKey();
@@ -22,5 +35,11 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    // this Method will return the relationship between user and contacts
+    public function contacts()
+    {
+        return $this->hasMany(Contacts::class);
     }
 }
